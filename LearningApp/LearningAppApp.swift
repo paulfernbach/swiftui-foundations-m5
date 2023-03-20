b11b3202e0f439a99b1394b3ec1791bea5433e63
Paul Fernbach <pfernbaach@hotmil.com>
2023-03-20 11:52:00 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Paul Fernbach on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
